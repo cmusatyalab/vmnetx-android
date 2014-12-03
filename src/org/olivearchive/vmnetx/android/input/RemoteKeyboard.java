5ef7e62461cfcef07152bc9a4e36426bea8c8dcd
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2014-12-02 19:24:22 -0500

    Ignore modifier key state flags from KeyEvents
    Only update the modifier state when we receive an actual press/release
    event for those keys.

--- src/org/olivearchive/vmnetx/android/input/RemoteKeyboard.java
+++ src/org/olivearchive/vmnetx/android/input/RemoteKeyboard.java
@@ -24,10 +24,6 @@ public class RemoteKeyboard {
     }
 
     public boolean processLocalKeyEvent(KeyEvent evt) {
-        return processLocalKeyEvent(evt, 0);
-    }
-    
-    private boolean processLocalKeyEvent(KeyEvent evt, int additionalMetaState) {
         int keyCode = evt.getKeyCode();
         //android.util.Log.e(TAG, evt.toString() + " " + keyCode);
 
@@ -45,7 +41,7 @@ public class RemoteKeyboard {
             }
 
             // Update the meta-state with writeKeyEvent.
-            int metaState = modifiers.getModifiers()|additionalMetaState|convertEventMetaState(evt);
+            int metaState = modifiers.getModifiers();
             spice.writeKeyEvent(keyCode, metaState, down);
             
             if (keyCode == 0 /*KEYCODE_UNKNOWN*/) {
@@ -53,7 +49,7 @@ public class RemoteKeyboard {
                 if (s != null) {
                     for (int i = 0; i < s.length(); i++) {
                         //android.util.Log.e(TAG, "Sending unicode: " + s.charAt(i));
-                        sendUnicode (s.charAt(i), metaState);
+                        sendUnicode(s.charAt(i));
                     }
                 }
                 return true;
@@ -107,7 +103,7 @@ public class RemoteKeyboard {
      * @param unicodeChar
      * @param metaState
      */
-    public void sendUnicode (char unicodeChar, int additionalMetaState) {
+    private void sendUnicode(char unicodeChar) {
         KeyCharacterMap fullKmap    = KeyCharacterMap.load(KeyCharacterMap.FULL);
         KeyCharacterMap virtualKmap = KeyCharacterMap.load(KeyCharacterMap.VIRTUAL_KEYBOARD);
         char[] s = new char[1];
@@ -122,32 +118,11 @@ public class RemoteKeyboard {
         if (events != null) {
             for (int i = 0; i < events.length; i++) {
                 KeyEvent evt = events[i];
-                processLocalKeyEvent(evt, additionalMetaState);
+                processLocalKeyEvent(evt);
             }
         } else {
             android.util.Log.e("RemoteKeyboard", "Could not use any keymap to generate KeyEvent for unicode: " + unicodeChar);
         }
-    }
-    
-    /**
-     * Converts event meta state to our meta state.
-     * @param event
-     * @return
-     */
-    private int convertEventMetaState (KeyEvent event) {
-        int metaState = 0;
-        int eventMetaState = event.getMetaState();
-        
-        // Add shift, ctrl, alt, and super to metaState if necessary.
-        if ((eventMetaState & 0x000000c1 /*KeyEvent.META_SHIFT_MASK*/) != 0)
-            metaState |= KeyEvent.META_SHIFT_ON;
-        if ((eventMetaState & 0x00007000 /*KeyEvent.META_CTRL_MASK*/) != 0)
-            metaState |= KeyEvent.META_CTRL_ON;
-        if ((eventMetaState & KeyEvent.META_ALT_MASK) !=0)
-            metaState |= KeyEvent.META_ALT_ON;
-        if ((eventMetaState & 0x00070000 /*KeyEvent.META_META_MASK*/) != 0)
-            metaState |= KeyEvent.META_META_ON;
-        return metaState;
     }
 
     private int keyCodeToModifierMask(int keyCode) {
